941eb642f4fb27391079a021a5cca67c55a58a3a
Vanessa Estrada <vestrada64@gmail.com>
2018-02-13 15:42:55 -0800

    Add score component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import GameBoard from './components/GameBoard/GameBoard';
 import ColorPicker from './components/ColorPicker/ColorPicker';
+import ScoreButton from './components/ScoreButton/ScoreButton';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <header className="App-header">React Mastermind</header>
       <GameBoard />
       <ColorPicker />
+      <ScoreButton />
       </div>
     );
   }
